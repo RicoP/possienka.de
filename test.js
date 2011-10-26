a05627fc6500919652e9ba45357c09ca6e9b6b49
Rico P <mailtorico@googlemail.com>
2011-10-26 14:11:55 +0000

    test entfernt

--- test.js
+++
@@ -1,5 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello 8002  \n');
-}).listen(8002, "127.0.0.1");
