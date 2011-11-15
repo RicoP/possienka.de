44e4a9d44d6862f562fd56a84594e8125d6424bf
Rico P Server <mailtorico@googlemail.com>
2011-11-15 15:47:22 +0000

    temp subdomain

--- proxy.js
+++ proxy.js
@@ -6,7 +6,9 @@ httpProxy.createServer({
     'possienka.de' : '127.0.0.1:8001',
     'www.possienka.de' : '127.0.0.1:8001',
     'gamejam1109.possienka.de' : '127.0.0.1:8003',  
-    'mmr.possienka.de' : '127.0.0.1:8004' 
+    'mmr.possienka.de' : '127.0.0.1:8004', 
+    'se2demo.possienka.de' : '127.0.0.1:8005',
+    'temp.possienka.de' : '127.0.0.1:8006' 
   }
 }).listen(80); 
 
